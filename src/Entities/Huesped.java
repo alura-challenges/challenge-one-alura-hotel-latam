bf263425f2d280fd87d6449376ba3d27d802403b
gfCrova <giancrova.cl5@gmail.com>
2023-04-30 16:58:45 -0300

    refactor model huesped

--- src/Entities/Huesped.java
+++ src/Entities/Huesped.java
@@ -1,20 +1,32 @@
 package Entities;
 
-import java.time.LocalDate;
+import java.sql.Date;
 
-public class Huespedes {
+public class Huesped {
 	private int id;
 	private String nombre;
 	private String apellido;
-	private LocalDate fechaNacimiento;
+	private Date fechaNacimiento;
+	private String nacionalidad;
 	private int telefono;
 	private int numeroReservas;
 	
 	
-	public Huespedes() {
+	public Huesped() {
 	}
 	
 	
+	public Huesped(int id, String nombre, String apellido, Date fechaNacimiento,  String nacionalidad, int telefono, int numeroReservas) {
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.telefono = telefono;
+		this.numeroReservas = numeroReservas;
+	}
+
+
 	public int getId() {
 		return id;
 	}
@@ -33,11 +45,17 @@ public class Huespedes {
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	public LocalDate getFechaNacimiento() {
+	public Date getFechaNacimiento() {
 		return fechaNacimiento;
 	}
-	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+	public void setFechaNacimiento(Date fechaNacimiento) {
 		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
 	}
 	public int getTelefono() {
 		return telefono;
@@ -50,6 +68,14 @@ public class Huespedes {
 	}
 	public void setNumeroReservas(int numeroReservas) {
 		this.numeroReservas = numeroReservas;
+	}
+
+
+	@Override
+	public String toString() {
+		return "ID: " + id + ", NOMBRE: " + nombre + ", APELLIDO: " + apellido + ", FECHA-NAC: "
+				+ fechaNacimiento + ", NACIONALIDAD: " + nacionalidad + ", TELEFONO: " + telefono + ", N° RESERVA: "
+				+ numeroReservas + "]";
 	}
 	
 	
