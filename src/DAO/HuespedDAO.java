38cc625eb456aa5c098b174ee2a2026dbbcc595a
gfCrova <giancrova.cl5@gmail.com>
2023-04-30 23:41:38 -0300

    update consultas a la db

--- src/DAO/HuespedDAO.java
+++ src/DAO/HuespedDAO.java
@@ -5,24 +5,28 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import Entities.Huesped;
-import Factory.ConnectionFactory;
 
-public class HuespedesDAO {
+public class HuespedDAO {
 	
 	Connection con;
 	
+	public HuespedDAO(Connection con) {
+		this.con = con;
+	}
+
+
 	public List<Huesped> listar() {
 
 		List<Huesped> lista = new ArrayList<>();
 		Huesped usuario = null;
-		
-		Connection con = new ConnectionFactory().recuperateConnection();
 
-		try (con) {
+		try {
 
 			final PreparedStatement statement = con.prepareStatement("SELECT * FROM huespedes");
 
@@ -36,19 +40,23 @@ public class HuespedesDAO {
 					
 					while (rs.next()) {
 						
-						int id = rs.getInt("id_huespedes");
+						Integer id = rs.getInt("id_huespedes");
 						String nombre = rs.getString("nombre");
 						String apellido = rs.getString("apellido");
-						Date fechaNacimiento = rs.getDate("fecha_nacimiento");
+						LocalDate fechaNacimiento = rs.getDate("fecha_nacimiento").toLocalDate();
 						String nacionalidad = rs.getString("nacionalidad");
-						int telefono = rs.getInt("telefono");
-						int numeroReserva = rs.getInt("id_reservas");
+						Integer telefono = rs.getInt("telefono");
+						Integer numeroReserva = rs.getInt("id_reservas");
 						
 						usuario = new Huesped(id, nombre, apellido, fechaNacimiento, nacionalidad, telefono, numeroReserva);
 						lista.add(usuario);
 						System.out.println(usuario);
-					}
-				}
+					} 
+				} catch(SQLException e) {
+					throw new RuntimeException(e);
+				} 
+			} catch(SQLException e) {
+				throw new RuntimeException(e);
 			}
 		} catch (SQLException e) {
 			throw new RuntimeException(e);
@@ -57,4 +65,94 @@ public class HuespedesDAO {
 	}
 
 	
+	
+	public void guardar(Huesped usuario) {
+
+    	try {
+
+			final PreparedStatement statement = con.prepareStatement("INSERT INTO huespedes"
+																+ "(nombre, apellido, fecha_nacimiento, nacionalidad, telefono, id_reservas) "
+																+ "VALUES (?, ?, ?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
+			try (statement) {
+				
+				statement.setString(1, usuario.getNombre());
+				statement.setString(2, usuario.getApellido());
+				statement.setObject(3, usuario.getFechaNacimiento());
+				statement.setString(4, usuario.getNacionalidad());
+				statement.setInt(5, usuario.getTelefono());
+				statement.setInt(6, usuario.getNumeroReservas());
+
+				statement.execute();
+				
+				final ResultSet rs = statement.getGeneratedKeys();
+
+				try (rs) {
+					while(rs.next()) {
+						usuario.setId(rs.getInt(1));
+						System.out.println("Fue insertado un nuevo huesped: " + usuario);
+					}
+				} catch(SQLException e) {
+					throw new RuntimeException(e);
+				}
+			} catch(SQLException e) {
+				throw new RuntimeException(e);
+			}
+
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	public int modificar(Integer id, String nombre, String apellido, LocalDate fecha_nac, String nacionalidad, Integer telefono) {
+
+		try {
+
+			final PreparedStatement statement = con.prepareStatement("UPDATE huespedes SET "
+					+ "nombre = ? "
+					+ ", apellido = ? "
+					+ ", fecha_nacimiento = ? "
+					+ ", nacionalidad = ? "
+					+ ", telefono = ? "
+					//+ ", id_reserva = ? "
+					+ " WHERE id_huespedes = ? ");
+
+			try (statement) {
+				
+				statement.setString(1, nombre);
+				statement.setString(2, apellido);
+				statement.setObject(3, fecha_nac);
+				statement.setString(4, nacionalidad);
+				statement.setInt(5, telefono);
+				//statement.setInt(6, idReserva);
+				statement.setInt(6, id);
+
+				statement.execute();
+
+				System.out.println("Se modificó con éxito el huesped: " + nombre);
+				return statement.getUpdateCount();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public int eliminar(Integer id) {
+
+		try {
+
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM huespedes WHERE id_huespedes = ?");
+
+			try (statement) {
+
+				statement.setInt(1, id);
+				statement.execute();
+
+				return statement.getUpdateCount();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
--- src/DAO/ReservasDAO.java
+++ src/DAO/ReservasDAO.java
@@ -1,5 +1,152 @@
 package DAO;
 
-public class ReservasDAO {
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
+import Entities.Huesped;
+import Entities.Reservas;
+
+public class ReservasDAO {
+	
+	Connection con;
+	
+	public ReservasDAO(Connection con) {
+		this.con = con;
+	}
+
+
+	public List<Reservas> listarReservas() {
+
+		List<Reservas> lista = new ArrayList<>();
+		Reservas reserva = null;
+
+		try {
+
+			final PreparedStatement statement = con.prepareStatement("SELECT * FROM reservas");
+
+			try (statement) {
+				
+				statement.execute();
+
+				final ResultSet rs = statement.getResultSet();
+
+				try (rs) {
+					
+					while (rs.next()) {
+						
+						int id = rs.getInt("id_reservas");
+						LocalDate fechaEntrada = rs.getDate("fecha_entrada").toLocalDate();
+						LocalDate fechaSalida = rs.getDate("fecha_salida").toLocalDate();
+						double valor = rs.getInt("valor");
+						String formaDePago = rs.getString("forma_de_pago");
+						
+						reserva = new Reservas(id, fechaEntrada, fechaSalida, valor, formaDePago);
+						lista.add(reserva);
+						System.out.println(reserva);
+					} 
+				} catch(SQLException e) {
+					throw new RuntimeException(e);
+				} 
+			} catch(SQLException e) {
+				throw new RuntimeException(e);
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+		return lista;
+	}
+
+	
+	
+	public void agregarReserva(Reservas reserva) {
+
+    	try {
+
+			final PreparedStatement statement = con.prepareStatement("INSERT INTO reservas"
+																+ "(fecha_entrada, fecha_salida, valor, forma_de_pago) "
+																+ "VALUES (?, ?, ?, ?);", Statement.RETURN_GENERATED_KEYS);
+			try (statement) {
+				
+				statement.setObject(1, reserva.getFechaEntrada());
+				statement.setObject(3, reserva.getFechaSalida());
+				statement.setDouble(2, reserva.getValor());
+				statement.setString(4, reserva.getFormaDePago());
+
+				statement.execute();
+				
+				final ResultSet rs = statement.getGeneratedKeys();
+
+				try (rs) {
+					while(rs.next()) {
+						reserva.setId(rs.getInt(1));
+						System.out.println("Se asgregó una nueva reserva: " + reserva);
+					}
+				} catch(SQLException e) {
+					throw new RuntimeException(e);
+				}
+			} catch(SQLException e) {
+				throw new RuntimeException(e);
+			}
+
+		} catch(SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	
+	public int modificarReserva(Integer id, LocalDate fechaEntrada, LocalDate fechaSalida, Double valor, String formaDePago) {
+
+		try {
+
+			final PreparedStatement statement = con.prepareStatement("UPDATE reservas SET "
+					+ "fecha_entrada = ? "
+					+ ", fecha_salida = ? "
+					+ ", valor = ? "
+					+ ", forma_de_pago = ? "
+					+ " WHERE id_reservas = ? ");
+
+			try (statement) {
+				
+				statement.setObject(1, fechaEntrada);
+				statement.setObject(2, fechaSalida);
+				statement.setDouble(3, valor);
+				statement.setString(4, formaDePago);
+				statement.setInt(5, id);
+
+				statement.execute();
+
+				System.out.println("Se modificó la reserva!");
+				return statement.getUpdateCount();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public int eliminarReserva(Integer id) {
+
+		try {
+
+			final PreparedStatement statement = con.prepareStatement("DELETE FROM reservas WHERE id_reservas = ?");
+
+			try (statement) {
+
+				statement.setInt(1, id);
+				statement.execute();
+
+				System.out.println("Se eliminó la reserva!");
+				return statement.getUpdateCount();
+			}
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
 }
