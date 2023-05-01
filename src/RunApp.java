870ac6114c71ce18371abe39f3c162bbc4bdbfff
gfCrova <giancrova.cl5@gmail.com>
2023-05-01 02:20:58 -0300

    separando el ejecutable de al app

---
+++ src/RunApp.java
@@ -0,0 +1,22 @@
+import java.awt.EventQueue;
+
+import views.MenuPrincipal;
+
+public class RunApp {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MenuPrincipal frame = new MenuPrincipal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+}
