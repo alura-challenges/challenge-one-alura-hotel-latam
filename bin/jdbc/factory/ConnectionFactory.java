3c6b3b26a253ed1810edb7cb1805833938334902
Mina Guevara <107224237+minaguevara@users.noreply.github.com>
2023-09-01 20:44:47 -0500

    Add files via upload

---
+++ bin/jdbc/factory/ConnectionFactory.java
@@ -0,0 +1,31 @@
+package jdbc.factory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class ConnectionFactory {
+
+	public DataSource dataSource;
+
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotelalura?useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("aluraLatam1234");
+		
+
+		this.dataSource = comboPooledDataSource;
+	}
+
+	public Connection recuperarConexion() {
+		try {
+			return this.dataSource.getConnection();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
